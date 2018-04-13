ff9634add18930b49d525ffaa5e5ca1fbb700a38
Sharon O'Malley <sharonomalley743@gmail.com>
2018-04-13 16:21:43 +0100

    JFrame implemented

--- Assignment/src/duo_vs/Launcher.java
+++ Assignment/src/duo_vs/Launcher.java
@@ -7,8 +7,10 @@ public class Launcher
 	public static void main(String[] args)
 	{
 		//store object in variable
-		
+		Game game = new Game("Duo-vs", 700, 550);
+				
 		//call class
+		game.start();
 
 	}
 	
