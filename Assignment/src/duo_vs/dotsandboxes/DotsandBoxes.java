ba9e2aafcd0327211002d7dc69ae90c7010d0667
lauren-byrne <c16452764@mydit.ie>
2018-04-24 13:27:31 +0100

    changed code to be more efficient and to include boolean array

--- Assignment/src/duo_vs/dotsandboxes/DotsandBoxes.java
+++ Assignment/src/duo_vs/dotsandboxes/DotsandBoxes.java
@@ -10,7 +10,7 @@ package duo_vs.dotsandboxes;
 //plug ins
 import java.awt.Color;
 import java.awt.Graphics;
-
+import java.util.ArrayList;
 //imported classes
 import duo_vs.Handler;
 
@@ -18,149 +18,91 @@ import duo_vs.Handler;
 public class DotsandBoxes 
 {	
 	private Handler handler;
-	private int[] savedXrow,savedYrow,savedXcol,savedYcol;
-	int iRow, jRow, iCol, jCol;
-	int l,s ;
-	boolean count, colcount;
-	int jRow1 =  jRow-1;
-	int jRow2 = jRow + 1;
-	int jCol1 = jCol -1;
-	int check;
-	
-	
-	
-	public DotsandBoxes(Handler handler)
-	{
-		savedXrow =new int[100];
-		savedYrow =new int[100];
-		savedXcol= new int[100];
-		savedYcol= new int[100];
-		this.handler =handler;
-		
-		
+	private boolean[][] boxes = new boolean[20][10];
+	public DotsandBoxes(Handler handler) {
+		this.handler = handler;
 	}
-	
-	
-	public void fill_lines() {
-		
-		for( int i = 25; i < 750; i += 50) 
-		{
-			for(int j = 100; j < 750; j+= 50 ) 
-			{
-				while(handler.getMouseManager().isLeftPressed() && handler.getMouseManager().getMouseX() >i && handler.getMouseManager().getMouseX() < i+50 && handler.getMouseManager().getMouseY() > j && handler.getMouseManager().getMouseY() < j+17)
-				{
-			
-						count = true;
-						iRow = i;
-						jRow = j;
-						System.out.print("");
-					
-				}
-	
-				
-				while(handler.getMouseManager().isLeftPressed() && handler.getMouseManager().getMouseX() >i && handler.getMouseManager().getMouseX() < i+17 && handler.getMouseManager().getMouseY() > j && handler.getMouseManager().getMouseY() < j+50)
-				{
-					
-					colcount = true;
-					iCol = i;
-					jCol = j;
-					System.out.print("");
-				}
-				
-			}
-		}
-		
-		if(count)
-		{
-			savedXrow[l]= iRow;
-			savedYrow[l]= jRow;
-			l = l +1;
-			count = false;
-		}
-		
-		if(colcount)
-		{
-			savedXcol[s]= iCol;
-			savedYcol[s]= jCol;
-			s =s +1;
-			colcount = false;
-		}
-	}
-
-	
-	
 	public void tick() 
 	{
-		fill_lines();
+		fillBoxes();
 	}
 	
-	
-
-		
-	
-	
-
-	public void render(Graphics g)
-	{
-		
-		g.setColor(Color.BLACK);
-		String str1 = "PLAYER 1";
-		String str2 = "PLAYER 2";
-		g.drawString(str1, 100, 50);
-		g.drawString(str2, 500, 50 );
-		
-		
-		
-		
-		// Dots
-		for(int i = 25; i < 750; i+=50)
+	public void render(Graphics g) {
+		drawDots(g);
+		drawLines(g);
+		updateFrame(g);
+	}
+	private void drawDots(Graphics g) {
+		for(int i = 25; i < 550; i+=50)
 		{
-			for(int j =100; j <750 ; j+=50)
+			for(int j =25; j < 550; j+=50)
 			{
 				g.drawOval( i, j, 15, 15);
 				g.fillOval(i, j, 15, 15);
 			}
 		}
-		
-		// Lines
-		for(int i = 25; i < 750; i+=50)
+	}
+	private void drawLines(Graphics g) {
+		for(int i = 25; i < 500; i+=50)
 		{
-			for(int j = 100; j < 750; j+=50) 
+			for(int j = 25; j < 500; j+=50) 
 			{
 				g.drawRect(i, j, 15, 50);
 				g.drawRect(i, j, 50, 15);
-			
-			}
-		}
-		
-		
-			for(int i=0; i<100; i++)
-			{
-				if(savedXrow[i]!=0 && savedYrow[i]!=0)
-				{		
-					g.fillRect(savedXrow[i], savedYrow[i], 65, 15);
-					
-					
+				if(i > 450) {
+					g.drawRect(i+50, j, 15, 50);
+				}
+				if(j > 450) {
+					g.drawRect(i, j+50, 50, 15);
 				}
 			}
-			
-			
-			
-			for(int i=0; i<100; i++)
-			{
-				if(savedXcol[i]!=0 && savedYcol[i]!=0)
-				{
-					g.fillRect(savedXcol[i], savedYcol[i], 15, 65);
-				}	
-			}	
-			
-		
-			
-			
 		}
-						
-						
- }	
+	}
+	private void fillBoxes() {
+		for( int i = 25, x = 0; i < 500; i += 50, x += 2) 
+		{
+			for(int j = 25, y = 0; j < 500; j+= 50, y ++) 
+			{
+				//Rows
+				while(handler.getMouseManager().isLeftPressed() && 
+						handler.getMouseManager().getMouseX() >i && 
+						handler.getMouseManager().getMouseX() < i+50 && 
+						handler.getMouseManager().getMouseY() > j &&
+						handler.getMouseManager().getMouseY() < j+15)
+				{
+					boxes[x][y] = true;
+					System.out.print("");
+
+				}	
+				//Cols
+				while(handler.getMouseManager().isLeftPressed() &&
+						handler.getMouseManager().getMouseX() >i &&
+						handler.getMouseManager().getMouseX() < i+15 &&
+						handler.getMouseManager().getMouseY() > j &&
+						handler.getMouseManager().getMouseY() < j+50)
+				{
+					boxes[x+1][y] = true;
+					System.out.print("");
+				}
+					
+			}
+		}
+	}
+	private void updateFrame(Graphics g) {
+		for(int i = 0; i <20; i += 2) {
+			for(int  j = 0; j < 10; j++){
+				if(boxes[i][j]) {
+						g.fillRect((i/2 * 50) + 25 , (j* 50) + 25, 50, 15);
+				}
+				if(boxes[i+1][j]) {
+					g.fillRect(((i/2) * 50) + 25 , (j* 50) + 25, 15, 50);
+				}
+			}
+		}
+	}
+	
+	
+}	
 
 
 
