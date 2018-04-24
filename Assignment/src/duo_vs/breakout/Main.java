3707329bd7b8cd9137c078ac2e965aaedbfa6691
Christian Maclachlan <chrismaclachlan97@gmail.com>
2018-04-24 12:04:26 +0100

    single player breakout 2

---
+++ Assignment/src/duo_vs/breakout/Main.java
@@ -0,0 +1,18 @@
+package duo_vs.breakout;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		Game game = new Game();
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Breakout ball");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(game);
+	}
+
+}
